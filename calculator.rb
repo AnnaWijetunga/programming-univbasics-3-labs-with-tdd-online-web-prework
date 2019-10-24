90e27b009bde069957a0c86f349d869019b8efda
Anna Wijetunga <annawijetunga@gmail.com>
2019-10-24 09:54:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,4 @@
 describe "./calculator.rb"
 calculator.rb = first_number
 first_number = get_variable_from_file('calculator.rb', "first_number")
+expect(first_number).to be_an(Integer).or be_a(Float)
